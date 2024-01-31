0360e648287a8b62a2b951f9f05c4195571fd166
Artem Vasilev <xartvdev@gmail.com>
2024-01-31 20:30:13 +0400

    typing models for typegoose

--- src/review/review.module.ts
+++ src/review/review.module.ts
@@ -1,7 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ReviewController } from './review.controller';
+import { TypegooseModule } from 'nestjs-typegoose';
+import { AuthModel } from 'src/auth/auth.model';
+import { ReviewModel } from './review.model';
 
 @Module({
   controllers: [ReviewController],
+  imports: [
+    TypegooseModule.forFeature([
+      {
+        typegooseClass: ReviewModel,
+        schemaOptions: {
+          collection: 'Review',
+        },
+      },
+    ]),
+  ],
 })
 export class ReviewModule {}
